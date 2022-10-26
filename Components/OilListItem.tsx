4830b64ce4c3de38b036f203599e0c5cfc1151a0
Yongveloper <aaa9015@naver.com>
2022-10-26 15:43:51 +0900

    [스타일링] 라디오 버튼 스타일링

--- Components/OilListItem.tsx
+++ Components/OilListItem.tsx
@@ -1,5 +1,10 @@
-import { Text, View } from 'react-native';
-import { RadioButton } from 'react-native-paper';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 interface IOilListItemProps {
   oil: string;
@@ -8,18 +13,78 @@ interface IOilListItemProps {
 }
 
 function OilListItem({ oil, selectedOil, setSelectedOil }: IOilListItemProps) {
+  const SelectOil = () => setSelectedOil(oil);
+
   return (
-    <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-      <RadioButton
-        value={oil}
-        status={selectedOil === oil ? 'checked' : 'unchecked'}
-        uncheckedColor="blue"
-        onPress={() => setSelectedOil(oil)}
-        color="#D9C832"
-      />
-      <Text style={{ color: 'white', fontSize: 18 }}>{oil}</Text>
-    </View>
+    <TouchableOpacity onPress={SelectOil} style={styles.container}>
+      <View style={styles.outSideRadioBtn}>
+        {oil === selectedOil && <View style={styles.insideRadioBtn} />}
+      </View>
+      <Text style={styles.oilName}>{oil}</Text>
+      <View>
+        {oil === '직접입력' ? (
+          <View style={styles.inputContainer}>
+            <TextInput
+              style={styles.input}
+              keyboardType="numeric"
+              placeholder="입력"
+              textAlign="right"
+              placeholderTextColor="grey"
+              onFocus={SelectOil}
+            />
+            <Text style={styles.wonText}>(원/L)</Text>
+          </View>
+        ) : (
+          <Text style={styles.wonText}>(약 ----원/L)</Text>
+        )}
+      </View>
+    </TouchableOpacity>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginBottom: 14,
+  },
+  outSideRadioBtn: {
+    height: 24,
+    width: 24,
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: '#D9C832',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  insideRadioBtn: {
+    height: 12,
+    width: 12,
+    borderRadius: 6,
+    backgroundColor: '#D9C832',
+  },
+  oilName: {
+    color: 'white',
+    fontSize: 20,
+    marginHorizontal: 40,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+  },
+  input: {
+    width: 80,
+    borderBottomColor: 'black',
+    borderBottomWidth: 3,
+    fontSize: 20,
+    color: 'white',
+    marginRight: 6,
+  },
+  wonText: {
+    color: 'white',
+    fontSize: 20,
+  },
+});
 
 export default OilListItem;
